8f77d003d163a7d1921370c75ce38983deb8784f
Cameron MacFarland <distantcam@gmail.com>
2017-08-30 00:48:39 +0800

    Added appveyor settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ let gulp  = require('gulp');
 let mustache = require("gulp-mustache");
 let rename = require("gulp-rename");
 let zip = require('gulp-zip');
+var exec = require('child_process').exec;
 let p = require('./package.json');
 
 let outputDir = './dist/' + p.name;
@@ -9,6 +10,8 @@ let outputDir = './dist/' + p.name;
 gulp.task('default', ['build']);
 
 gulp.task('build', ['zip-mod']);
+
+gulp.task('appveyor', ['set-appveyor-version', 'zip-mod']);
 
 gulp.task('build-info', () =>
     gulp.src('./info.mustache')
@@ -27,3 +30,13 @@ gulp.task('zip-mod', ['build-info', 'copy-src-files'], () =>
         .pipe(zip(p.name + '_' + p.version + '.zip'))
         .pipe(gulp.dest('dist'))
 );
+
+gulp.task('set-appveyor-version', (cb) =>
+{
+    exec('appveyor UpdateBuild -Version "' + p.version + '+build$APPVEYOR_BUILD_NUMBER"', (err) => {
+        if (err) return cb(err);
+        cb();
+    });
+    return cb();
+});
+
